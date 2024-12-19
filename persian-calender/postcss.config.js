212ece99e54e56a971c80d946b9c2c48b51ba25f
Mohammad sd <mamdbio1386.com>
2024-12-19 17:09:16 +0330

    fixed bug

--- persian-calender/postcss.config.js
+++ persian-calender/postcss.config.js
@@ -1,8 +1,7 @@
-// postcss.config.cjs
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
+// module.exports = {
+//     plugins: {
+//       tailwindcss: {},
+//       autoprefixer: {},
+//     },
+//   }
   
